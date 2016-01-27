65d54ce99adba6529ded1c74cae2e9b9b6f438d9
gcornut <gcornut@gmail.com>
2016-01-27 18:33:31 +0100

    Refactor code for better jQuery embedding and loading of multiple JSON ontology files.

--- src/cropOntologyWidget.js
+++ src/cropOntologyWidget.js
@@ -1,21 +1,21 @@
 /*
  * Copyright (c) 2015, INRA
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *  * Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
- * 
+ *
  *  * Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  *  * Neither the name of trait-ontology-widget nor the names of its
  *    contributors may be used to endorse or promote products derived from
  *    this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -25,369 +25,244 @@
  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
- * 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  * https://opensource.org/licenses/BSD-3-Clause
  */
 
 /**
- * @author Cyril Pommier, Raphael Flores
- * 
- */
-/*
- * 
- * Extension of code from http://alexmarandon.com/articles/web_widget_jquery/  
- * 
- */
-
-/*
+ * @author Cyril Pommier, Raphael Flores, Guillaume Cornut
+ *
+ * Extension of code from http://alexmarandon.com/articles/web_widget_jquery/
  * TODO : try to do this with shadow DOM
  */
-/*
-(function(){
-	
+(function () {
+	"use strict";
 
-})();
-*/
-(
-		function(factory) {
-			"use strict";
-			if (typeof define === 'function' && define.amd) {
-				define([ 'jquery' ], factory);
-			} else if (typeof module !== 'undefined' && module.exports) {
-				module.exports = factory(require('jquery'));
-			} else {
-				factory(jQuery);
-			}
+	window.errors = []
+	function dump() { errors.push(arguments);console.error(arguments); }
+
+	// Prevent multiple load
+	if (window.CropOntologyWidget) return;
+
+	/*
+	 * Async load appropriate jQuery version without external conflict
+	 */
+	function loadjQuery(main) {
+		function scriptLoadHandler() {
+			main(window.jQuery.noConflict(true));
 		}
-		
-
-		(
-				function($, undefined) {
-					"use strict";
-					//TODO: This var must be set according to the enclosing page. To adjust
-//					var libLocalPath="../common/javaScript/brapiOntoWidget/"
-					//var libLocalPath=location.pathname+"/";
-					
-
-					// prevent another load? maybe there is a better way?
-					if ($.cropOntologyWidget) {
-						return;
-					}
-
-					// Localize jQuery variable
-					var jQuery;
-					var cropOntologyTreeWidgetSrc ;
-					var showCheckBoxes;
-					var brapiCropOntoWidgetJs;
-					var useSearchField;
-
-					$.cropOntologyWidget = {
-							version : '1',
-
-							defaults : {
-								cropOntologyTreeWidgetSrc : "Provide json URL",
-								brapiCropOntoWidgetJs : location.pathname,
-								showCheckBoxes : false,
-								useSearchField : false
-							}
-					};
-
-					function libraryLoader() {
-		/** ****** Load jQuery if not present ******** */
-//		if (window.jQuery === undefined || window.jQuery.fn.jquery !== '1.11.3') {
 		if (window.jQuery === undefined || window.jQuery.fn.jquery !== '1.11.3') {
 			var script_tag = document.createElement('script');
 			script_tag.setAttribute("type", "text/javascript");
-			script_tag.setAttribute("src", brapiCropOntoWidgetJs+"/lib/jquery.js");
-			// TODO: use min version of the lib ASAP
+			script_tag.setAttribute("src", "https://code.jquery.com/jquery-1.11.3.js");
+
 			if (script_tag.readyState) {
-				script_tag.onreadystatechange = function() { // For old  versions of IE
-					if (this.readyState == 'complete'
-						|| this.readyState == 'loaded') {
+				// For old  versions of IE
+				script_tag.onreadystatechange = function() {
+					if (this.readyState == 'complete' || this.readyState == 'loaded') {
 						scriptLoadHandler();
 					}
 				};
-			} else { // Other browsers
-				script_tag.onload = scriptLoadHandler();
-
+			} else {
+				// Other browsers
+				script_tag.onload = scriptLoadHandler;
 			}
-			// Try to find the head, otherwise default to the
-			// documentElement
 			(document.getElementsByTagName("head")[0] || document.documentElement)
-			.appendChild(script_tag);
-			loadJsTree();
+				.appendChild(script_tag);
 		} else {
-			// The jQuery version on the window is the one we want to
-			// use
-			jQuery = window.jQuery;
-			loadJsTree();
+			main(window.jQuery);
+		}
+	}
+
+	function loadCSS($, href) {
+		$("<link>", {rel: "stylesheet", type: "text/css", href: href})
+			.appendTo('head');
+	}
+
+	function dirname(path) {
+		var dir = path.replace(/\/[^\/]*\/?$/, '');
+		if (dir === path) return "";
+		return dir;
+	}
+
+	var defaultOptions = {
+		showCheckBoxes: false,
+		useSearchField: false
+	}
+
+	/*
+	 * Initialize CropOntologyWidget with jQuery
+	 */
+	function initialize($, widget) {
+		// Find script base path
+		$("script").each(function (i, script) {
+			var path = $(script).attr("src");
+			if (path && path.match(/cropOntologyWidget(\.min)?\.js$/)) {
+				widget.basePath = dirname(path);
+				return false;
+			}
+		});
+		if (widget.basePath === undefined) {
+			console.log("ERROR: Cannot initialize CropOntologyWidget. Unable to find base path.");
+			return;
 		}
 
+		widget.$root = $(widget.selector);
+		if (widget.$root.size() === 0) {
+			console.log("ERROR: Cannot initialize CropOntologyWidget. Cannot find element '"+selector+"'.");
+			return;
+		}
+
+		// Build Components
+		widget.$root.addClass("ontology-widget");
+		widget.$treeBox = $('<div class="treeBox" ></div>');
+		widget.$root.append(widget.$treeBox);
+
+		widget.$treeBox.append('<h2>Traits, methods and scales</h2>');
+
+		widget.$searchBox = $('<div class="searchBox"></div>');
+		widget.$treeBox.append(widget.$searchBox);
+
+
+		widget.$tree = $('<div class="tree"></div>');
+		widget.$treeBox.append(widget.$tree);
+
+		widget.$details = $('<div class="details"></div>');
+		widget.$details.append('<h2>Details</h2>');
+		widget.$root.append(widget.$details);
+
+		widget.$detailList = $('<dl></dl>');
+		widget.$details.append(widget.$detailList);
+		//$details.append('<div id ="brapiCropOntoWidget-details" class="details-content"></div>');
+
+		// Load required CSS
+		loadCSS($, widget.basePath + "/lib/themes/default/style.min.css");
+		loadCSS($, widget.basePath + "/cropOntologyWidget.css");
+
+		// Load JS tree
+		var oldjQuery = window.jQuery;
+		var old$ = window.$;
+		window.jQuery = window.$ = $;
+		$.ajaxSetup({async: false, cache: true});
+		$.getScript(widget.basePath + "/lib/jstree.min.js", function () {
+			$.ajaxSetup({async: true, cache: false});
+			window.jQuery =oldjQuery;
+			window.$ = old$;
+
+			// Load data
+			loadData($, widget);
+		});
 	}
-	
-	window.libraryloader = libraryLoader;
-	function loadJsTree(){
-		/** ****** Load jStree if not present ******** */
-		/*
-		 * DRY violated here to clean when possible
-		 */
-		// window.jQuery likely mistake, use $ ?
-//		if (jQuery.jstree === undefined
-//		|| jQuery.jstree.version !== '3.2.1') {
-		var script_tag_jstree = document.createElement('script');
-		script_tag_jstree.setAttribute("type", "text/javascript");
-		script_tag_jstree.setAttribute("src", brapiCropOntoWidgetJs+"/lib/jstree.min.js");
-		if (script_tag_jstree.readyState) {
-			script_tag_jstree.onreadystatechange = function() { // For
-				// old
-				// versions of
-				// IE
-				if (this.readyState == 'complete'
-					|| this.readyState == 'loaded') {
-					scriptLoadHandler();
+
+	function loadData($, widget) {
+		var ontologyTerms = [];
+
+		// Load Ontology repository JSON
+		$.getJSON(
+			widget.ontologyRepositoryFile
+		).then(function(ontologyRepository) {
+			// Load all ontology JSON from ontology repository
+			return $.when.apply($, $.map(ontologyRepository, function(ontology) {
+				return $.getJSON(widget.ontologyBasePath+"/"+ontology, function (terms) {
+					$.map(terms, function(term) {
+						ontologyTerms.push(term);
+					});
+				});
+			}));
+		}).then(function () {
+			// Ontologies loaded
+
+			var coreOptions = {
+				"data": ontologyTerms,
+				"themes": {
+					"variant": "large", "icons": false,
+					"stripes": true, "expand_selected_onload": true
 				}
 			};
-		} else { // Other browsers
-			script_tag_jstree.onload = scriptLoadHandler;
+			var checkBoxOptions = {
+				"keep_selected_style": false,
+				"visible": widget.showCheckBoxes
+			}
+
+			if (!widget.useSearchField) {
+				widget.$tree.jstree({
+					"core": coreOptions,
+					"checkbox": checkBoxOptions,
+					"plugins": ["checkbox"]
+				});
+			} else {
+				var $input = $('<input placeholder="Search terms..." class="treeSearch" type="text">');
+				widget.$searchBox.append($input);
+				widget.$tree.jstree({
+					"core": coreOptions,
+					"checkbox": checkBoxOptions,
+					"search": {
+						"show_only_matches": true
+					},
+					"plugins": ["checkbox", "search"]
+				});
+
+				var to = false;
+				$input.keyup(function() {
+					if (to) clearTimeout(to);
+
+					to = setTimeout(function() {
+						var v = $input.val();
+						widget.$tree.jstree(true).search(v);
+					}, 250);
+				});
+			}
+
+			widget.$tree.on('activate_node.jstree', function (_, data) {
+				widget.clearDetails();
+				var fullNode = widget.$tree.jstree(true).get_node(data.node.id);
+				$.each(fullNode.data, widget.appendDetails);
+			});
+		});
+	}
+
+	window.CropOntologyWidget = function(selector, options) {
+		this.version = '1';
+		this.basePath;
+
+		// Components
+		this.selector = selector;
+		this.$root;
+		this.$tree;
+		this.$treeBox;
+		this.$searchBox;
+		this.$details;
+		this.$detailList;
+
+		// Options
+		this.showCheckBoxes = options.showCheckBoxes || defaultOptions.showCheckBoxes;
+		this.useSearchField = options.useSearchField || defaultOptions.useSearchField;
+		this.ontologyRepositoryFile = options.ontologyRepositoryFile;
+		if(!this.ontologyRepositoryFile) {
+			console.log("ERROR: Cannot initialize CropOntologyWidget. Missing parameter 'ontologyRepositoryFile'.");
+			return;
 		}
-		// Try to find the head, otherwise default to the
-		// documentElement
-		(document.getElementsByTagName("head")[0] || document.documentElement)
-		.appendChild(script_tag_jstree);
-//		} else {
-//		// The jQuery version on the window is the one we want to
-//		// use
-//		jQuery.jstree = jQuery.jstree;
-////		main();
-//		}
-		/** ***** Load CSS ****** */
-		var css_link = $(
-				"<link>",
-				{
-					rel : "stylesheet",
-					type : "text/css",
-					href : brapiCropOntoWidgetJs+"/lib/themes/default/style.min.css"
-				});
-		css_link.appendTo('head');
-		var css_link = $(
-				"<link>",
-				{
-					rel : "stylesheet",
-					type : "text/css",
-					href : brapiCropOntoWidgetJs+"/cropOntologyWidget.css"
-				});
-		css_link.appendTo('head');
+		this.ontologyBasePath = dirname(this.ontologyRepositoryFile);
 
+		// Public methods
+		this.clearDetails = function() {
+			widget.$detailList.empty();
+		}
+		this.appendDetails = function(text, data) {
+			if (data) {
+				widget.$detailList.append("<dt>" + text + "</dt><dd>" + data + "</dd>");
+			}
+		}
+		this.getSelectedNodeIds = function(){
+			return widget.$tree.jstree('get_selected');
+		}
+
+		// Delayed initialization with jQuery
+		var widget = this;
+		loadjQuery(function($) { initialize($, widget) });
+
+		// End of CropOntologyWidget constructor
+		return this;
 	}
-	window.loadJsTree = loadJsTree;
-
-//	libraryLoader();
-	
-	/** ****** Called once jQuery has loaded ***** */
-	function scriptLoadHandler() {
-		// Restore $ and window.jQuery to their previous values and
-		// store the
-		// new jQuery in our local jQuery variable
-		jQuery = window.jQuery.noConflict(true);
-		// Call our main function
-		// main();
-	}
-					
-
-					$.fn.cropOntologyWidget = function(arg) {
-
-						// check for string argument
-//						var is_method = (typeof arg === 'string'), args = Array.prototype.slice
-//						.call(arguments, 1), result = null;
-//						if (arg === true && !this.length) {
-//						return false;
-//						}
-//						this.each(function() {
-//						// get the instance (if there is one) and method (if it
-//						// exists)
-//						var instance = $.jstree.reference(this), method = is_method
-//						&& instance ? instance[arg] : null;
-//						// if calling a method, and method is available - execute on
-//						// the instance
-//						result = is_method && method ? method.apply(instance, args)
-//						: null;
-//						// if there is no instance and no method is being called -
-//						// create one
-//						if (!instance && !is_method
-//						&& (arg === undefined || $.isPlainObject(arg))) {
-//						$.jstree.create(this, arg);
-//						}
-//						// if there is an instance and no method is called - return
-//						// the instance
-//						if ((instance && !is_method) || arg === true) {
-//						result = instance || false;
-//						}
-//						// if there was a method call which returned a result -
-//						// break and return the value
-//						if (result !== null && result !== undefined) {
-//						return false;
-//						}
-//						});
-						// Restore $ and window.jQuery to their previous values and
-						// store the
-						// new jQuery in our local jQuery variable
-						// already done correcly in library loader
-//						jQuery = window.jQuery.noConflict(true);
-
-						if (arg.brapiCropOntoWidgetJs !== undefined){
-							brapiCropOntoWidgetJs = arg.brapiCropOntoWidgetJs;
-						}
-						if (arg.cropOntologyTreeWidgetSrc !== undefined){
-							cropOntologyTreeWidgetSrc = arg.cropOntologyTreeWidgetSrc;
-						}
-						if (arg.showCheckBoxes !== undefined){
-							showCheckBoxes = arg.showCheckBoxes;
-						}
-						if (arg.useSearchField !== undefined){
-							useSearchField = arg.useSearchField;
-						}
-						
-
-						libraryLoader();
-
-						// Call our main function
-						main();
-						// if there was a method call with a valid return value - return
-						// that, otherwise continue the chain
-//						return result !== null && result !== undefined ? result : this;
-					};
-
-
-
-					/** ****** Our main function ******* */
-					function main() {
-						jQuery(document)
-						.ready(function($) {
-							// TODO: All library handling must be perfected, Have a look
-							// at bower for that.
-
-							
-
-							/** ***** Load tree ****** */
-							var request = $.getJSON(cropOntologyTreeWidgetSrc);
-							var ontologyJson;
-
-							request.complete(function() {
-
-								ontologyJson = request.responseText;
-
-								// 6 create an instance when the
-								// DOM is ready
-								$('#brapiCropOntoWidget').addClass("ontology-widget");
-								$('#brapiCropOntoWidget').append('<div id ="brapiCropOntoWidget-tree-box" class="treeBox" ></div>');
-								$('#brapiCropOntoWidget-tree-box').append('<h2>Traits, methods and scales</h2>');
-								
-								if (!useSearchField){
-									$('#brapiCropOntoWidget-tree-box').append('<div id ="brapiCropOntoWidget-tree" class="tree" ></div>');
-									$('#brapiCropOntoWidget-tree')
-									.jstree(
-											{
-												"core" : {
-													'data' : $
-													.parseJSON(ontologyJson),
-													"themes" : {
-														"variant" : "large",
-														"icons" : false,
-														"stripes" : true,
-														"expand_selected_onload" : true
-													}
-												},
-												"checkbox" : {
-													"keep_selected_style" : false,
-													"visible" : showCheckBoxes
-												},
-												"plugins" : ["checkbox" ]
-											});
-									//"wholerow", ca be slow
-								}else{
-									$('#brapiCropOntoWidget-tree-box').append('<input id="plugins4_q" value="" class="input" style="margin:0em auto 1em auto; display:block; padding:4px; border-radius:4px; border:1px solid silver;" type="text">');
-									$('#brapiCropOntoWidget-tree-box').append('<div id ="brapiCropOntoWidget-tree" class="tree" ></div>');
-									$('#brapiCropOntoWidget-tree')
-									.jstree(
-											{
-												"core" : {
-													'data' : $
-													.parseJSON(ontologyJson),
-													"themes" : {
-														"variant" : "large",
-														"icons" : false,
-														"stripes" : true,
-														"expand_selected_onload" : true
-													}
-												},
-												"checkbox" : {
-													"keep_selected_style" : false,
-													"visible" : showCheckBoxes
-												},
-												"search":{
-													 "show_only_matches":true
-												},
-												"plugins" : ["checkbox", "search"]
-											});
-									
-									var to = false;
-									$('#plugins4_q').keyup(function () {
-									    if(to) { clearTimeout(to); }
-									    to = setTimeout(function () {
-									      var v = $('#plugins4_q').val();
-									      $('#brapiCropOntoWidget-tree').jstree(true).search(v);
-									    }, 250);
-									  });
-								}
-							
-								
-								// On click display details
-								$('#brapiCropOntoWidget-tree')
-														.on('activate_node.jstree',
-																function(node,
-																		data) {
-																	displayDetails(data);
-																});
-
-							});
-							$('#brapiCropOntoWidget').append('<div id ="brapiCropOntoWidget-details-box" class="details"></div>');
-							$('#brapiCropOntoWidget-details-box').append('<h2>Details</h2>');
-							$('#brapiCropOntoWidget-details-box').append('<div id ="brapiCropOntoWidget-details" class="details-content"></div>');
-							
-
-						});
-					}
-					
-					function displayDetails(data){
-						var fullNode = $('#brapiCropOntoWidget-tree')
-						.jstree(true).get_node(data.node.id);
-						$('#brapiCropOntoWidget-details').empty();
-						appendDetails("Variable name", fullNode.data.VariableName);
-						appendDetails("Variable ID", fullNode.data.VariableID);
-						appendDetails("Short name", fullNode.data.VariableShortName);
-						appendDetails("Description", fullNode.data.Description);
-						appendDetails("Unit", fullNode.data.Unit);
-						//Display all
-						$.each( fullNode.data, function( key, value ) {
-							appendDetails( key ,  value );
-							});
-					}
-					
-					function appendDetails(text, data){
-						if (data != null){
-							$('#brapiCropOntoWidget-details').append('<div class="detail-element"><span>'+text+": </span> "+data+"</div>");
-						}
-					}
-
-					function getSelectedNodeIds(){
-						return $("#brapiCropOntoWidget-tree").jstree('get_selected');
-					}
-
-					window.getSelectedNodeIds = getSelectedNodeIds;
-
-				}
-		)
-);
-
-
+})();
