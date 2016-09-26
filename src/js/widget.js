c8ee78c338cef4f5f443f0606b166c07c1db3d5c
gcornut <gcornut@gmail.com>
2016-09-26 12:17:00 +0200

    Fix text overflow in variable details view

--- src/js/widget.js
+++ src/js/widget.js
@@ -201,13 +201,26 @@ global.CropOntologyWidget = function(selector, options) {
 	function clearDetails() {
 		widget.$detailList.empty();
 	}
+	function formatKey(str) {
+		return str
+			// split camel case: insert a space before all caps
+			.replace(/([A-Z][a-z])/g, ' $1')
+			.toLowerCase().trim()
+			// uppercaser first letter
+			.replace(/^./, function(str){ return str.toUpperCase(); })
+	}
 	function appendDetails(text, data, optionalPrefix) {
 		var prefix = optionalPrefix || "";
 		if (data) {
 			if (typeof data === 'string') {
-				var key = prefix + text;
+				var key = formatKey(prefix + text);
 				var value = data;
-				widget.$detailList.append("<dt>" + key + "</dt><dd>" + data + "</dd>");
+				widget.$detailList.append(
+					'<div class="detail">'+
+						'<div class="key">' + key + '</div>'+
+						'<div class="value">' + data + '</div>'+
+					'</div>'
+				);
 			} else if (typeof data === 'object') {
 				$.each(data, function(t, d) {
 					return appendDetails(t, d, prefix + text + " - ");
