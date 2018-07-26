e40e4675dd06d497086cdb7de09d94ad0b7f5494
gcornut <gcornut@gmail.com>
2018-07-26 16:51:20 +0200

    Refactor widget code to Ecmascript 2015 with babel transcoding.

---
+++ src/js/tree/TreeBuilder.js
@@ -0,0 +1,192 @@
+const $ = require('jquery')
+
+import { Arrays } from '../utils'
+import { BreedingAPIClient } from '../api/BreedingAPIClient'
+
+export class TreeBuilder {
+
+  constructor(widget) {
+    this.widget = widget
+    this.breedingAPIClient = new BreedingAPIClient(widget.breedingAPIEndpoint)
+
+    this.ontologyDbIds = []
+    this.traitClassIds = []
+    this.traitIds = []
+    this.allNodeIds = []
+
+    this.deferredNodeIds = $.Deferred()
+  }
+
+  /**
+   * Deferred list of all node identifiers (resolved when the tree has
+   * been entirely built with `this.buildTree()`)
+   */
+  getAllNodeIds() {
+    return this.deferredNodeIds
+  }
+
+  /**
+   * Asynchronously loads jstree nodes from Breeding API ontologies & varaibles
+   */
+  buildTree(callback) {
+    // Fetch all ontologies
+    let ontologiesRequest = this.breedingAPIClient.fetchOntologies()
+
+    // Fetch all variables
+    let variablesRequest = this.breedingAPIClient.fetchVariables()
+
+    // Ontology list request worked (load ontology & then variables)
+    ontologiesRequest.done((ontologies) => {
+      let rootNodes = $.map(ontologies, (ontology) => {
+          let ontologyRootNode = this.ontologyAsRootNode(ontology)
+          this.ontologyDbIds.push(ontologyRootNode.id)
+          this.allNodeIds.push(ontologyRootNode.id)
+          return ontologyRootNode
+      })
+      callback(rootNodes)
+      this.displayVariables(variablesRequest)
+    })
+
+    // Ontology list request failed (just load variables without ontology metadata)
+    ontologiesRequest.fail(() => {
+      // Display empty tree
+      callback([])
+      // Load variables separatly because jstree can't handle more than 1500 nodes at once
+      this.displayVariables(variablesRequest)
+    })
+  }
+
+  displayVariables(variablesRequest) {
+    // Wait for jstree to be ready & Variables to be loaded
+    var ready = $.when(this.widget.jsTreePanel.treeReady(), variablesRequest)
+
+    // Success
+    ready.done((readyEvent, variables) => {
+
+      let childNodes = $.map(variables, (variable) => this.variableAsNodes(variable))
+      // Add nodes to tree
+      $.map(childNodes, (childNode) => {
+        this.allNodeIds.push(childNode.id)
+        this.widget.jsTreePanel.jstree.create_node(childNode.parent, childNode)
+      })
+
+      this.deferredNodeIds.resolve(this.allNodeIds)
+    })
+
+    // Failure
+    ready.fail(function() {
+      callback([]); //Load empty tree
+      this.widget.detailsPanel.displayError(
+        'An error occured while contacting Breeding API endpoint: ' +
+        this.widget.breedingAPIEndpoint
+      )
+    })
+  }
+
+  /**
+   * Transforms a Breeding API variable into jstree nodes for the trait class,
+   * the trait and variable
+   */
+  variableAsNodes(variable) {
+    var nodes = []
+
+    // Skip variables that are not in english (null values are accepted)
+    // Language selection is planned (issue gnpis/trait-ontology-widget#4)
+    if (variable.language && variable.language.toUpperCase() !== 'EN') {
+      return nodes
+    }
+
+    var baseNodeData = {
+      'ontologyDbId': variable['ontologyDbId'],
+      'ontologyName': variable['ontologyName']
+    }
+
+    var ontologyDbId = variable.ontologyDbId
+    // Test to avoid duplicate ontology node
+    if (!Arrays.contains(this.ontologyDbIds, ontologyDbId)) {
+      this.ontologyDbIds.push(ontologyDbId)
+
+      // Add ontology node
+      nodes.push(ontologyAsRootNode(baseNodeData))
+    }
+
+    var variableParent = ontologyDbId
+
+    // Variable display name
+    var variableText = variable.name
+    if (variable.synonyms && variable.synonyms.length) {
+      variableText += ': ' + variable.synonyms[0]
+    }
+
+    // If has a trait
+    var trait = variable.trait
+    if (trait) {
+      var traitParent = ontologyDbId
+      var traitId = trait.traitDbId
+      variableParent = traitId
+
+      // If has a trait class
+      var traitClass = trait.class
+      if (traitClass) {
+        traitClass = traitClass.trim()
+        var traitClassId = ontologyDbId + ':' + traitClass
+        traitParent = traitClassId
+
+        // Test to avoid duplicate trait class node
+        if (!Arrays.contains(this.traitClassIds, traitClassId)) {
+          this.traitClassIds.push(traitClassId)
+
+          // Add trait class node
+          nodes.push({
+            'id': traitClassId,
+            'parent': ontologyDbId,
+            'text': traitClass,
+            'data': $.extend({}, baseNodeData, {
+              'name': traitClass,
+            }),
+            'type': 'traitClass'
+          })
+        }
+      }
+
+      // Test to avoid duplicate trait node
+      if (!Arrays.contains(this.traitIds, traitId)) {
+        this.traitIds.push(traitId)
+
+        // Add trait node
+        nodes.push({
+          'id': traitId,
+          'parent': traitParent,
+          'text': trait.name,
+          'data': $.extend({}, baseNodeData, trait),
+          'type': 'trait'
+        })
+      }
+    }
+
+    // Add variable node
+    nodes.push({
+      'id': variable.observationVariableDbId,
+      'parent': variableParent,
+      'text': variableText,
+      'data': variable,
+      'type': 'variable',
+    })
+
+    return nodes
+  }
+
+
+  /**
+   * Transforms a Breeding API ontology into a jstree root node
+   */
+  ontologyAsRootNode(ontology) {
+    return {
+      'id': ontology.ontologyDbId,
+      'parent': '#',
+      'text': ontology.ontologyName,
+      'data': ontology,
+      'type': 'ontology'
+    }
+  }
+}
--- src/js/ui/JSTreePanel.js
+++ src/js/ui/JSTreePanel.js
@@ -1,270 +1,267 @@
-var $ = require('jquery');
-require("jstree");
+const $ = require('jquery')
+require('jstree')
 
-var Arrays = require('../utils').Arrays;
+import { Arrays } from '../utils'
+import { TreeBuilder } from '../tree/TreeBuilder'
+import { SearchField } from './SearchField'
 
-var TreeBuilder = require('../TreeBuilder');
-var SearchField = require('./SearchField');
+export class JSTreePanel {
+  constructor(widget) {
+    // Initialize the tree builder
+    this.treeBuilder = new TreeBuilder(widget)
 
-module.exports = function JSTreePanel (widget) {
-  var jsTreePanel = this;
-
-  // Initialize the tree builder
-  var treeBuilder = new TreeBuilder(widget);
-
-  // Base jstree options
-  var jsTreeOptions = {
-    "core": {
-      "check_callback" : true,
-      "themes": {
-        "variant": "large", "icons": false,
-        "stripes": true, "expand_selected_onload": true
+    // Base jstree options
+    this.jsTreeOptions = {
+      'core': {
+        'check_callback' : true,
+        'themes': {
+          'variant': 'large', 'icons': false,
+          'stripes': true, 'expand_selected_onload': true
+        },
+        'data': (self, callback) => {
+          this.treeBuilder.buildTree(callback.bind(self))
+        }
       },
-      "data": function(self, callback) {
-        treeBuilder.buildTree(callback.bind(self));
+      'checkbox': {
+        'keep_selected_style': false,
+        'visible': widget.showCheckBoxes
+      },
+      // Define types of nodes
+      'types': {
+        'ontology': {
+          'valid_children': [ 'trait', 'traitClass', 'variable' ],
+          'a_attr': { 'class': 'ontology labeled' }
+        },
+        'traitClass': {
+          'valid_children': [ 'trait', 'variable' ],
+          'a_attr': { 'class': 'traitClass labeled' }
+        },
+        'trait': {
+          'valid_children': [ 'variable' ],
+          'a_attr': { 'class': 'trait labeled' }
+        },
+        'variable': {
+          'valid_children': [],
+          'a_attr': { 'class': 'variable labeled' }
+        }
+      },
+      'plugins': [ 'checkbox', 'types', 'sort' ]
+    }
+
+    // Initialize panel div
+    this.$treeBox = $('<div class="treeBox"></div>')
+
+    // Add the title
+    var $title = $('<h2>Traits, methods and scales</h2>')
+    this.$treeBox.append($title)
+
+    // Initialize search field if requested
+    this.searchField = null
+    if (widget.useSearchField) {
+      this.searchField = new SearchField(widget)
+      this.jsTreeOptions['search'] = {
+        'show_only_matches': true,
+        'search_callback': this.searchField.nodeMatchesText
       }
-    },
-    "checkbox": {
-      "keep_selected_style": false,
-      "visible": widget.showCheckBoxes
-    },
-    // Define types of nodes
-    "types": {
-      "ontology": {
-        "valid_children": [ "trait", "traitClass", "variable" ],
-        "a_attr": { "class": "ontology labeled" }
-      },
-      "traitClass": {
-        "valid_children": [ "trait", "variable" ],
-        "a_attr": { "class": "traitClass labeled" }
-      },
-      "trait": {
-        "valid_children": [ "variable" ],
-        "a_attr": { "class": "trait labeled" }
-      },
-      "variable": {
-        "valid_children": [],
-        "a_attr": { "class": "variable labeled" }
-      }
-    },
-    "plugins": [ "checkbox", "types", "sort" ]
-  };
+      this.jsTreeOptions['plugins'].push('search')
 
-  // Initialize panel div
-  var $treeBox = $('<div class="treeBox" ></div>');
+      // Add the search field
+      this.$treeBox.append(this.searchField.getElement())
+    }
 
-  // Add the title
-  var $title = $('<h2>Traits, methods and scales</h2>');
-  $treeBox.append($title);
+    // Add the jstree div
+    this.$tree = $('<div class="tree"></div>')
+    this.$treeBox.append(this.$tree)
 
-  // Initialize search field if requested
-  var searchField;
-  if (widget.useSearchField) {
-    searchField = new SearchField(widget);
-    jsTreeOptions["search"] = {
-      "show_only_matches": true,
-      "search_callback": searchField.nodeMatchesText
-    };
-    jsTreeOptions["plugins"].push("search");
+    this.customSelectionHandlers = []
+    this.customClickHandlers = []
+    this.jstree = null
 
-    // Add the search field
-    $treeBox.append(searchField.getElement());
+    /**
+     * Delegate method
+     * Get deferred list of node ids
+     */
+    this.getAllNodeIds = () => this.treeBuilder.getAllNodeIds()
   }
 
-  // Add the jstree div
-  var $tree = $('<div class="tree"></div>');
-  $treeBox.append($tree);
-
-  var customSelectionHandlers = [];
-  var customClickHandlers = [];
-  this.jstree = null;
-
-  // Methods below
-
   /**
-   * Initializes jsTree
-   */
-  this.initializeJSTree = function() {
+  * Initializes jsTree
+  */
+  initializeJSTree() {
     // Initializes logic for custom click & selection event handling on jstree
     // nodes
-    $tree.on('click', '.jstree-anchor', function (event) {
-      var $target = $(event.target);
-      var $targetItem = $target.parents('li').eq(0);
+    this.$tree.on('click', '.jstree-anchor', (event) => {
+      var $target = $(event.target)
+      var $targetItem = $target.parents('li').eq(0)
 
-      var targetNodeId = $targetItem.attr('id');
-      var targetNode = widget.jsTreePanel.jstree.get_node(targetNodeId);
+      var targetNodeId = $targetItem.attr('id')
+      var targetNode = this.jstree.get_node(targetNodeId)
 
-      $.map(customClickHandlers, function(clickHandler) {
-        clickHandler($targetItem, targetNode);
-      });
+      $.map(this.customClickHandlers, function(clickHandler) {
+        clickHandler($targetItem, targetNode)
+      })
 
       if (!$target.is('.jstree-checkbox')) {
         // Click on node (not checkbox)
 
         //prevent node selection, just display details
-        event.stopImmediatePropagation();
+        event.stopImmediatePropagation()
       } else {
         // Click on checkbox => selection handling
 
-        $.map(customSelectionHandlers, function(selectionHandler) {
+        $.map(this.customSelectionHandlers, function(selectionHandler) {
           setTimeout(function() {
-            selectionHandler($targetItem, targetNode);
-          }, 0);
-        });
+            selectionHandler($targetItem, targetNode)
+          }, 0)
+        })
       }
-      event.preventDefault();
-    });
+      event.preventDefault()
+    })
 
     // Initializes jsTree
-    $tree.jstree(jsTreeOptions);
-    jsTreePanel.jstree = $tree.jstree(true);
+    this.$tree.jstree(this.jsTreeOptions)
+    this.jstree = this.$tree.jstree(true)
   }
 
   /**
-   * Returns the jQuery element
-   */
-  this.getElement = function() {
-    return $treeBox;
+  * Returns the jQuery element
+  */
+  getElement() {
+    return this.$treeBox
   }
 
   /**
-   * Adds a selection handler to the jstree nodes
-   */
-  this.addSelectionChangeHandler = function(handler) {
-    customSelectionHandlers.push(handler);
+  * Adds a selection handler to the jstree nodes
+  */
+  addSelectionChangeHandler(handler) {
+    this.customSelectionHandlers.push(handler)
   }
 
   /**
-   * Adds a click handler on jstree nodes
-   */
-  this.addClickHandler = function(handler) {
-    customClickHandlers.push(handler);
+  * Adds a click handler on jstree nodes
+  */
+  addClickHandler(handler) {
+    this.customClickHandlers.push(handler)
   }
 
   /**
-   * Returns a deferred value that is resolved when the tree has loaded
-   */
-  this.treeReady = function() {
-    var deferred = $.Deferred();
-    $tree.on('ready.jstree', deferred.resolve);
-    return deferred;
+  * Returns a deferred value that is resolved when the tree has loaded
+  */
+  treeReady() {
+    var deferred = $.Deferred()
+    this.$tree.on('ready.jstree', deferred.resolve)
+    return deferred
   }
 
   /**
-   * Reset node selection
-   */
-  this.resetSelection = function() {
-    jsTreePanel.jstree.deselect_all();
+  * Reset node selection
+  */
+  resetSelection() {
+    this.jstree.deselect_all()
   }
 
   /**
-   * Reset the jsTreePanel (reset search field, node selection, opened nodes)
-   */
-  this.reset = function() {
-    if (searchField) searchField.clear();
-    jsTreePanel.resetSelection();
-    jsTreePanel.showAll();
-    jsTreePanel.jstree.close_all();
+  * Reset the jsTreePanel (reset search field, node selection, opened nodes)
+  */
+  reset() {
+    if (this.searchField) this.searchField.clear()
+    this.resetSelection()
+    this.showAll()
+    this.jstree.close_all()
   }
 
   /**
   * Show all nodes
   */
-  this.showAll = function () {
-    jsTreePanel.jstree.show_all();
+  showAll() {
+    this.jstree.show_all()
   }
 
   /**
-   * Hide all nodes
-   */
-  this.hideAll = function () {
-    jsTreePanel.jstree.hide_all();
+  * Hide all nodes
+  */
+  hideAll() {
+    this.jstree.hide_all()
   }
 
   /**
-   * Search node identifiers using a predicate on nodes
-   */
-  this.searchNodeIds = function(nodes, predicate) {
-    var filtered = [];
-    $.map(nodes, function(node) {
+  * Search node identifiers using a predicate on nodes
+  */
+  searchNodeIds(nodes, predicate) {
+    var filtered = []
+    $.map(nodes, (node) => {
       if (predicate(node)) {
-        filtered.push(node.id);
+        filtered.push(node.id)
       }
       if (node.children) {
-        filtered = filtered.concat(jsTreePanel.searchNodeIds(node.children, predicate));
+        filtered = filtered.concat(this.searchNodeIds(node.children, predicate))
       }
-    });
-    return filtered;
+    })
+    return filtered
   }
 
   /**
   * Hide all nodes except the given nodes, their parents and their children
   */
-  this.showOnly = function(requiredNodeIds) {
-    treeBuilder.getAllNodeIds().then(function(allNodeIds) {
-      var shownNodeIds = [];
-      var hiddenNodeIds = [];
-      $.map(allNodeIds, function(nodeId) {
+  showOnly(requiredNodeIds) {
+    this.treeBuilder.getAllNodeIds().then((allNodeIds) => {
+      var shownNodeIds = []
+      var hiddenNodeIds = []
+      $.map(allNodeIds, (nodeId) => {
         if(requiredNodeIds.indexOf(nodeId) !== -1) {
-          var node = jsTreePanel.jstree.get_node(nodeId);
-          shownNodeIds.push(nodeId);
-          if (node.children_d) shownNodeIds = shownNodeIds.concat(node.children_d);
-          if (node.parents) shownNodeIds = shownNodeIds.concat(node.parents);
+          var node = this.jstree.get_node(nodeId)
+          shownNodeIds.push(nodeId)
+          if (node.children_d) shownNodeIds = shownNodeIds.concat(node.children_d)
+          if (node.parents) shownNodeIds = shownNodeIds.concat(node.parents)
         } else {
-          hiddenNodeIds.push(nodeId);
+          hiddenNodeIds.push(nodeId)
         }
-      });
+      })
       // using setTimeout to delay DOM modifications (reducing UI blocking)
-      setTimeout(function() {
-        $.map(hiddenNodeIds, function(nodeId) {
+      setTimeout(() => {
+        $.map(hiddenNodeIds, (nodeId) => {
           if(!Arrays.contains(shownNodeIds, nodeId)) {
-            jsTreePanel.jstree.hide_node(nodeId);
-            jsTreePanel.jstree.deselect_node(nodeId);
+            this.jstree.hide_node(nodeId)
+            this.jstree.deselect_node(nodeId)
           }
-        });
-      }, 0);
-      setTimeout(function() {
-        $.map(shownNodeIds, function(nodeId) {
-          jsTreePanel.jstree.show_node(nodeId);
-        });
-      }, 0);
-    });
+        })
+      }, 0)
+      setTimeout(() => {
+        $.map(shownNodeIds, (nodeId) => {
+          this.jstree.show_node(nodeId)
+        })
+      }, 0)
+    })
   }
 
   /**
-   * Set selected nodes by id
-   */
-  this.setSelectedNodeIds = function(nodeIds) {
-    jsTreePanel.resetSelection();
+  * Set selected nodes by id
+  */
+  setSelectedNodeIds(nodeIds) {
+    this.resetSelection()
     if (nodeIds && nodeIds.length) {
-      treeBuilder.getAllNodeIds().then(function() {
-        $.map(nodeIds, function(nodeId) {
-          jsTreePanel.jstree.select_node(nodeId);
-        });
-      });
+      this.treeBuilder.getAllNodeIds().then(() => {
+        $.map(nodeIds, (nodeId) => {
+          this.jstree.select_node(nodeId)
+        })
+      })
     }
   }
 
   /**
-   * Get identifiers of selected nodes
-   */
-  this.getSelectedNodeIds = function() {
-    return jsTreePanel.jstree.get_selected();
+  * Get identifiers of selected nodes
+  */
+  getSelectedNodeIds() {
+    return this.jstree.get_selected()
   }
 
   /**
-   * Get identifiers of selected leaf nodes
-   */
-  this.getSelectedLeafIds = function() {
-    return $.grep(jsTreePanel.getSelectedNodeIds(), function(id) {
-      var node = jsTreePanel.jstree.get_node(id);
-      return (!node.children.length && node.state.selected);
-    });
+  * Get identifiers of selected leaf nodes
+  */
+  getSelectedLeafIds() {
+    return $.grep(this.getSelectedNodeIds(), (id) => {
+      var node = this.jstree.get_node(id)
+      return (!node.children.length && node.state.selected)
+    })
   }
-
-  /**
-   * Get deferred list of node ids
-   */
-  this.getAllNodeIds = treeBuilder.getAllNodeIds;
-
-};
+}
