949ae420ccae42a758a1dc83498e25e44ce8e4a5
gcornut <guillaume.cornut@inra.fr>
2018-10-04 14:14:06 +0200

    Update distribution files.

--- dist/cropOntologyWidget.js
+++ dist/cropOntologyWidget.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:759b852e4d7b89cf925dbe0147b6bbf938c3e6d3ac4a1a11bf4996c93cb0bd7f
-size 1660069
+oid sha256:e885692d54a16a5a83737b8ca7dfad73cb5c8df11f0804ace92554440c080e1d
+size 1660130
