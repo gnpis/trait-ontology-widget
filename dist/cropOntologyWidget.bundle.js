b8cebd94bb3f454fde4f3c3325018ed6c8e9d178
gcornut <guillaume.cornut@inra.fr>
2018-12-13 16:00:08 +0100

    Generate a NPM module in dist folder when running build.

--- dist/cropOntologyWidget.bundle.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3c0611f6a3353342d1c6705a517d6db604836b5ccedc7ff8770a68e5573dd931
-size 1715338
--- dist/cropOntologyWidget.js
+++ dist/cropOntologyWidget.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d754628b7a84a58aa9beee928e78dc28bc235dd2ae32f594639f1e4aaaef4280
-size 1714952
+oid sha256:f11ffb74fece3155e6f2d7865f21994ddce6f27215213434dc8497b2c299c06e
+size 1714956
